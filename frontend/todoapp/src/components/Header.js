93e5c2c3ba2217fc392e6fb0ae823557e0eb5a04
Abraham Bueno <abueno@localhost.localdomain>
2019-04-10 14:16:51 -0500

    playing around with styles

--- frontend/todoapp/src/components/Header.js
+++ frontend/todoapp/src/components/Header.js
@@ -1,50 +1,68 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { withRouter } from 'react-router'
-import { AUTH_TOKEN } from '../constants'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
+import { AUTH_TOKEN } from "../constants";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
+  input: {
+    display: "none"
+  }
+});
 class Header extends Component {
-    render() {
-        const authToken = localStorage.getItem(AUTH_TOKEN)
-        return (
-            <div className="flex pa1 justify-between nowrap orange">
-                {/*<div className="flex flex-fixed black">*/}
-                    {/*<div className="fw7 mr1">Hacker News</div>*/}
-                    {/*<Link to="/" className="ml1 no-underline black">*/}
-                        {/*new*/}
-                    {/*</Link>*/}
-                    {/*<div className="ml1">|</div>*/}
-                    {/*<Link to="/search" className="ml1 no-underline black">*/}
-                        {/*search*/}
-                    {/*</Link>*/}
-                    {/*{authToken && (*/}
-                        {/*<div className="flex">*/}
-                            {/*<div className="ml1">|</div>*/}
-                            {/*<Link to="/create" className="ml1 no-underline black">*/}
-                                {/*submit*/}
-                            {/*</Link>*/}
-                        {/*</div>*/}
-                    {/*)}*/}
-                {/*</div>*/}
-                <div className="flex flex-fixed">
-                    {authToken ? (
-                        <div
-                            className="ml1 pointer black"
-                            onClick={() => {
-                                localStorage.removeItem(AUTH_TOKEN)
-                                this.props.history.push(`/`)
-                            }}
-                        >
-                            logout
-                        </div>
-                    ) : (
-                        <Link to="/login" className="ml1 no-underline black">
-                            login
-                        </Link>
-                    )}
-                </div>
+  render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN);
+    const { classes } = this.props;
+    return (
+      <div className="flex pa1 justify-between nowrap orange">
+        {/*<div className="flex flex-fixed black">*/}
+        {/*<div className="fw7 mr1">Hacker News</div>*/}
+        {/*<Link to="/" className="ml1 no-underline black">*/}
+        {/*new*/}
+        {/*</Link>*/}
+        {/*<div className="ml1">|</div>*/}
+        {/*<Link to="/search" className="ml1 no-underline black">*/}
+        {/*search*/}
+        {/*</Link>*/}
+        {/*{authToken && (*/}
+        {/*<div className="flex">*/}
+        {/*<div className="ml1">|</div>*/}
+        {/*<Link to="/create" className="ml1 no-underline black">*/}
+        {/*submit*/}
+        {/*</Link>*/}
+        {/*</div>*/}
+        {/*)}*/}
+        {/*</div>*/}
+        <div className="flex flex-fixed">
+          {authToken ? (
+            <div
+              className="ml1 pointer black"
+              onClick={() => {
+                localStorage.removeItem(AUTH_TOKEN);
+                this.props.history.push(`/`);
+              }}
+            >
+              logout
             </div>
-        )
-    }
+          ) : (
+            <Link to="/login">
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                login
+              </Button>
+            </Link>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withRouter(Header)
+export default withRouter(withStyles(styles)(Header));
--- frontend/todoapp/src/components/Login.js
+++ frontend/todoapp/src/components/Login.js
@@ -1,159 +1,167 @@
-import React, { Component } from 'react'
-import { AUTH_TOKEN } from '../constants'
-import { Mutation } from 'react-apollo'
-import gql from 'graphql-tag'
-import { withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
+import React, { Component } from "react";
+import { AUTH_TOKEN } from "../constants";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import { withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import "../styles/Login.css";
 const styles = theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-    },
-    dense: {
-        marginTop: 16,
-    },
-    menu: {
-        width: 200,
-    },
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  header: {
+    textAlign: "center"
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  dense: {
+    marginTop: 16
+  },
+  menu: {
+    width: 200
+  },
+  button: {
+    marginTop: "1em"
+  }
 });
 
-const currencies = [
-    {
-        value: 'USD',
-        label: '$',
-    },
-    {
-        value: 'EUR',
-        label: '€',
-    },
-    {
-        value: 'BTC',
-        label: '฿',
-    },
-    {
-        value: 'JPY',
-        label: '¥',
-    },
-];
 const SIGNUP_MUTATION = gql`
-    mutation SignupMutation($email: String!, $password: String!, $name: String!, $secret: String!) {
-        signup(email: $email, password: $password, name: $name, secret: $secret) {
-            token
-        }
+  mutation SignupMutation(
+    $email: String!
+    $password: String!
+    $name: String!
+    $secret: String!
+  ) {
+    signup(email: $email, password: $password, name: $name, secret: $secret) {
+      token
     }
-`
+  }
+`;
 
 const LOGIN_MUTATION = gql`
-    mutation LoginMutation($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-        }
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
     }
-`
+  }
+`;
 class Login extends Component {
-    state = {
-        login: true, // switch between Login and SignUp
-        email: '',
-        password: '',
-        secret:'',
-        name: '',
-    }
-    handleChange = e => {
-        e.preventDefault()
-        this.setState({[e.target.name] : e.target.value})
-    }
+  state = {
+    login: true, // switch between Login and SignUp
+    email: "",
+    password: "",
+    secret: "",
+    name: ""
+  };
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    render() {
-        const { login, email, password, name, secret } = this.state
-        const { classes } = this.props;
-        return (
-            <div>
-                <h4 className="mv3">{login ? 'Login' : 'Sign Up'}</h4>
-                <div className="flex flex-column">
-                    <form className={classes.container} noValidate autoComplete="off">
-                    {!login && (
+  render() {
+    const { login, email, password, name, secret } = this.state;
+    const { classes } = this.props;
+    return (
+      <div className={classes.header}>
+        <h4 className="mv3">{login ? "Login" : "Sign Up"}</h4>
+        <div className="flex flex-column">
+          <form className={classes.container} noValidate autoComplete="off">
+            {!login && (
+              <TextField
+                id="outlined-name"
+                label="Your name"
+                className={classes.textField}
+                value={name}
+                onChange={this.handleChange}
+                margin="normal"
+                variant="outlined"
+              />
+            )}
 
-                        <TextField
-                        id="outlined-name"
-                        label="Your name"
-                        className={classes.textField}
-                        value={name}
-                        onChange={this.handleChange}
-                        margin="normal"
-                        variant="outlined"
-                        />
-                    )}
-                    
-                        <TextField
-                            id="outlined-name"
-                            label="Your email address"
-                            className={classes.textField}
-                            value={email}
-                            onChange={this.handleChange}
-                            margin="normal"
-                            variant="outlined"
-                        />
+            <TextField
+              id="outlined-name"
+              label="Your email address"
+              className={classes.textField}
+              value={email}
+              onChange={this.handleChange}
+              margin="normal"
+              variant="outlined"
+            />
 
-                        <TextField
-                            id="outlined-name"
-                            label="Choose a safe password"
-                            className={classes.textField}
-                            value={password}
-                            onChange={this.handleChange}
-                            margin="normal"
-                            variant="outlined"
-                        />
+            <TextField
+              id="outlined-name"
+              label="Choose a safe password"
+              className={classes.textField}
+              value={password}
+              onChange={this.handleChange}
+              margin="normal"
+              variant="outlined"
+            />
 
-                        <TextField
-                            id="outlined-name"
-                            label="Choose a safe secret"
-                            className={classes.textField}
-                            value={secret}
-                            onChange={this.handleChange}
-                            margin="normal"
-                            variant="outlined"
-                        />
-                    </form>
-                </div>
-                <div className="flex mt3">
-                    <Mutation
-                        mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
-                        variables={{ email, password, name, secret }}
-                        onCompleted={data => this._confirm(data)}
-                    >
-                        {mutation => (
-                            <div className="pointer mr2 button" onClick={mutation}>
-                                {login ? 'login' : 'create account'}
-                            </div>
-                        )}
-                    </Mutation>
-                    <div
-                        className="pointer button"
-                        onClick={() => this.setState({ login: !login })}
-                    >
-                        {login
-                            ? 'need to create an account?'
-                            : 'already have an account?'}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+            <TextField
+              id="outlined-name"
+              label="Choose a safe secret"
+              className={classes.textField}
+              value={secret}
+              onChange={this.handleChange}
+              margin="normal"
+              variant="outlined"
+            />
+          </form>
+        </div>
+        <div className="flex mt3">
+          <Mutation
+            mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+            variables={{ email, password, name, secret }}
+            onCompleted={data => this._confirm(data)}
+          >
+            {mutation => (
+              <div className="pointer mr2 button" onClick={mutation}>
+                {login ? (
+                  <Button variant="contained" className={classes.button}>
+                    login
+                  </Button>
+                ) : (
+                  <Button variant="contained" className={classes.button}>
+                    "create account"
+                  </Button>
+                )}
+              </div>
+            )}
+          </Mutation>
+          <div
+            className="toggle-option"
+            onClick={() => this.setState({ login: !login })}
+          >
+            {login ? (
+              <Button variant="contained" className={classes.button}>
+                need to create an account?
+              </Button>
+            ) : (
+              <Button variant="contained" className={classes.button}>
+                already have an account?
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-    _confirm = async data => {
-        const { token } = this.state.login ? data.login : data.signup
-        this._saveUserData(token)
-        console.log('token: ', token)
-        this.props.history.push(`/`)
-    }
-    _saveUserData = token => {
-        localStorage.setItem(AUTH_TOKEN, token)
-    }
+  _confirm = async data => {
+    const { token } = this.state.login ? data.login : data.signup;
+    this._saveUserData(token);
+    console.log("token: ", token);
+    this.props.history.push(`/`);
+  };
+  _saveUserData = token => {
+    localStorage.setItem(AUTH_TOKEN, token);
+  };
 }
 
-export default withStyles(styles)(Login)
+export default withStyles(styles)(Login);
